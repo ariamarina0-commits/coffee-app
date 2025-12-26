98467a36a0912225f4c463d3bf531eda8f2bc167
ariamarina0-commits <ariamarina0@gmail.com>
2025-12-26 23:03:15 +0200

    fix: tailwind paths

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -2,9 +2,10 @@ import type { Config } from "tailwindcss";
 
 const config: Config = {
   content: [
-    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
+    "./app/**/*.{js,ts,jsx,tsx,mdx}",        // Case 1: app is in root
+    "./pages/**/*.{js,ts,jsx,tsx,mdx}",      // Case 2: pages are in root
+    "./components/**/*.{js,ts,jsx,tsx,mdx}", // Case 3: components are in root
+    "./src/**/*.{js,ts,jsx,tsx,mdx}",        // Case 4: everything is in src
   ],
   theme: {
     extend: {
