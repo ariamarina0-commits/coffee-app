b59934b14b070f46c73ba0832617d2531e4e6825
ariamarina0-commits <ariamarina0@gmail.com>
2025-12-31 20:06:39 +0200

    center icons

--- app/components/CoffeeAnimation.tsx
+++ app/components/CoffeeAnimation.tsx
@@ -9,11 +9,12 @@ interface CoffeeProps {
 
 export default function CoffeeAnimation({ src, className }: CoffeeProps) {
   return (
-    <div className={className}>
+    <div className={`${className} flex items-center justify-center mx-auto`}>
       <DotLottieReact
         src={src}
         loop
         autoplay
+        style={{ width: '100%', height: '100%' }} // Ensures it fills the container
       />
     </div>
   );
